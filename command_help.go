42546f52e0fa45c45d7409f1cb52567e4dfbb2c6
Sumitydv <sssssyyyyy749@gmail.com>
2024-05-08 05:15:32 +0530

    intiaize

---
+++ command_help.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func callbackHelp(args []string) error {
+
+	commands := GetCommands()
+
+	fmt.Println()
+	fmt.Println("\tWelcome to the Pokedex!")
+	fmt.Println()
+	fmt.Println("\tUsage:")
+	fmt.Println()
+
+	for _, c := range commands {
+		fmt.Printf("\t- %s: %s\n", c.name, c.description)
+	}
+
+	fmt.Println()
+
+	return nil
+}

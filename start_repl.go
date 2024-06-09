27abb0d5fcc4010b845844b92da90e6471511097
Sumitydv <sssssyyyyy749@gmail.com>
2024-06-09 08:55:32 +0530

    added all command

---
+++ start_repl.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func([]string) error
+}
+
+func start_repl() {
+
+	commands := GetCommands()
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+
+		words := strings.Fields(text)
+
+		if len(words) == 0 {
+			continue
+		}
+
+		command := words[0]
+
+		c, ok := commands[command]
+
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		c.callback(words)
+
+	}
+}
